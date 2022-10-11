e3ee45bd607e69d051ab5475e3d7f6b46d652205
Saket Verma <53481055+saketverma9@users.noreply.github.com>
2022-10-12 01:51:34 +0530

    Update minmax.cpp

--- cpp/minmax.cpp
+++ cpp/minmax.cpp
@@ -14,7 +14,6 @@ struct Pair getMinMax(int arr[], int n)
 {
 	struct Pair minmax;	
 	int i;
-	
 	// If there is only one element
 	// then return it as min and max both
 	if (n == 1)
@@ -23,7 +22,6 @@ struct Pair getMinMax(int arr[], int n)
 		minmax.min = arr[0];	
 		return minmax;
 	}
-	
 	// If there are more than one elements,
 	// then initialize min and max
 	if (arr[0] > arr[1])
@@ -36,12 +34,10 @@ struct Pair getMinMax(int arr[], int n)
 		minmax.max = arr[1];
 		minmax.min = arr[0];
 	}
-	
 	for(i = 2; i < n; i++)
 	{
 		if (arr[i] > minmax.max)	
 			minmax.max = arr[i];
-			
 		else if (arr[i] < minmax.min)	
 			minmax.min = arr[i];
 	}
