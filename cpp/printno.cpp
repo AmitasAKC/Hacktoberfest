8bfd47dca66b14f54511ddebeee5b9eab9d5cb25
Saket Verma <53481055+saketverma9@users.noreply.github.com>
2022-10-12 02:24:52 +0530

    Update printno.cpp

--- cpp/printno.cpp
+++ cpp/printno.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main() {    
     int number;
-
     cout << "Enter an integer: ";
     cin >> number;
-
-    cout << "You entered " << number;    
+    cout << "You entered " << number<<endl;    
     return 0;
 }
